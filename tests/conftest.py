45fac32ce046734460be4070c80998a83fe9efc9
piotrekmalpa10-spec <piotrekmalpa10@gmail.com>
2025-10-29 14:59:42 +0100

    Add files via upload

---
+++ tests/conftest.py
@@ -0,0 +1,28 @@
+import pytest
+import requests
+
+@pytest.fixture
+def base():
+    return 'https://zmyslonyadres.com'
+
+class Todos:
+    def __init__(self, base):
+        self.base = base
+
+
+    def register(self, name, gmail, password):
+        return requests.post(f'{self.base}/register', json={'username': name, 'gmail': gmail, 'password': password})
+
+    def login(self, name, gmail, password):
+        return requests.post(f'{self.base}/login', json={'username': name, 'gmail': gmail, 'password': password})
+    
+
+    def add_post(self, content, header):
+        return requests.post(f'{self.base}/posts', json={'content': content}, headers=header)
+    
+    def get_post_by_id(self, id, header):
+        return requests.get(f'{self.base}/posts/{id}', headers=header)
+    
+    def get_all_posts(self, header):
+        return requests.get(f'{self.base}/posts/', headers=header)
+    
---
+++ tests/test_main.py
@@ -0,0 +1,42 @@
+from conftest import Todos, base
+import pytest
+
+@pytest.fixture
+def clt(base):
+    Todos(base)
+
+
+def header(clt):
+    try:
+        reg = clt.register('maciek', 'maciek@gmail.com', 'maciek1234')
+    except:
+        r = reg.status_code
+        if r == 400:
+            pass
+
+    log = clt.login('maciek', 'maciek@gmail.com', 'maciek1234')
+
+    tok = log.json()['token']
+
+    heade = {'Authorization': f'Bearer {tok}'}
+
+    return heade
+
+
+def test_add_posts(clt, header):
+    add = clt.add_post('Post', header)
+    pj = add.json()
+
+    pid = pj['id']
+
+    gpi = clt.get_post_by_id(pid, header)
+
+    assert gpi == {'content': 'Post'}
+
+def test_all_posts(clt, header):
+    get = clt.get_all_posts(header)
+    getjson = get.json()
+
+    assert any('content' in g for g in getjson)
+
+
